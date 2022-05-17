95d6bcf58455e9dffbfc14fe52592666f94409cf
chrisrandall <chris.f.randall@gmail.com>
2022-05-17 09:37:58 -0600

    commit tree bd487cadd19eac8ab86d018e960848c764486a1d

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    const newCatsArray = [...cats];
+    newCatsArray.push(name);
+    return newCatsArray;
+}
+
+function prependCat(name){
+    const newCatsArray = [...cats];
+    newCatsArray.unshift(name);
+    return newCatsArray;
+}
+
+function removeLastCat(){
+    const newCatsArray = [...cats];
+    newCatsArray.pop();
+    return newCatsArray;
+}
+
+function removeFirstCat(){
+    const newCatsArray = [...cats];
+    newCatsArray.shift();
+    return newCatsArray;
+}
